7f58375df4cf19a1b302d1afbbc61c483f2bf854
zhangjingxing <robertjeremy@gapp.nthu.edu.tw>
2023-06-19 15:31:31 +0800

    Update minimax_player.cpp

--- src/player/minimax_player.cpp
+++ src/player/minimax_player.cpp
@@ -42,7 +42,7 @@ void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
   while(true) {
     // Choose a random spot.
-    auto move = minimax_player::get_move(root, 0);
+    auto move = minimax_player::get_move(root, 4);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
