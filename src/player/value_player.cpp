14dc14102cc9e643923eaeba9de1b8d701708eed
zhangjingxing <robertjeremy@gapp.nthu.edu.tw>
2023-06-19 21:21:11 +0800

    Update value_player.cpp

--- src/player/value_player.cpp
+++ src/player/value_player.cpp
@@ -42,7 +42,7 @@ void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
   while(true) {
     // Choose a random spot.
-    auto move = value_player::get_move(root, 0);
+    auto move = value_player::get_move(root);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
